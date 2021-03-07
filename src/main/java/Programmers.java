9ab1c896a33c8d07f27369b2f54c011eb834bd6f
Alisa Harodnik <alisa.solovchuk@mail.ru>
2021-03-07 15:31:00 +0300

    Fixed

--- src/main/java/Programmers.java
+++ src/main/java/Programmers.java
@@ -1,16 +1,22 @@
 public class Programmers {
     public static void main(String[] args) {
-        int amountOfProgrammers = 301;
-        switch (amountOfProgrammers) {
-            case 2: case 3: case 4:
+        int amountOfProgrammers = 181;
+        int caseNumber = 0;
+        if (amountOfProgrammers % 10 == 1 & amountOfProgrammers % 100 != 11) {
+            caseNumber = 1;
+        } else if (amountOfProgrammers % 10 >= 2 & amountOfProgrammers % 10 <= 4
+                & (amountOfProgrammers % 100 < 10 | amountOfProgrammers % 100 > 20)) {
+            caseNumber = 2;
+        }
+        switch (caseNumber) {
+            case 1:
+                System.out.println("Программист");
+                break;
+            case 2:
                 System.out.println("Программиста");
                 break;
             default:
-                if (amountOfProgrammers % 10 == 1){
-                    System.out.println("Программист");
-                } else {
-                    System.out.println("Программистов");
-                }
+                System.out.println("Программистов");
                 break;
         }
     }
--- src/main/java/ProgrammersVersionTwo.java
+++ src/main/java/ProgrammersVersionTwo.java
@@ -1,9 +1,10 @@
 public class ProgrammersVersionTwo {
     public static void main(String[] args) {
-        int amountOfProgrammers = 1;
-        if (amountOfProgrammers % 10 == 1) {
+        int amountOfProgrammers = 214;
+        if (amountOfProgrammers % 10 == 1 & amountOfProgrammers % 100 != 11) {
             System.out.println("Программист");
-        } else if (amountOfProgrammers > 1 & amountOfProgrammers < 5){
+        } else if (amountOfProgrammers % 10 >= 2 & amountOfProgrammers % 10 <= 4
+                & (amountOfProgrammers % 100 < 10 | amountOfProgrammers % 100 > 20)) {
             System.out.println("Программиста");
         } else {
             System.out.println("Программистов");
